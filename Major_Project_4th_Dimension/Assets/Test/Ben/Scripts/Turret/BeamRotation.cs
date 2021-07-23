8d2805d5deb48fd318df66c0efb589a2d4296362
Ben <bennybill16@gmail.com>
2021-07-23 14:18:00 +1000

    Added a change in colour for the detection so player knows they are hit
    It changes red for if the player gets hit, it defaults at green to say to the player that they haven't been hit yet. The materials can be changed later down the track.

--- Major_Project_4th_Dimension/Assets/Test/Ben/Scripts/Turret/BeamRotation.cs
+++ Major_Project_4th_Dimension/Assets/Test/Ben/Scripts/Turret/BeamRotation.cs
@@ -5,25 +5,42 @@ using UnityEngine;
 public class BeamRotation : Singleton<BeamRotation>
 {
     [SerializeField] private float beamLength = 10.0f;
-
+    [SerializeField] private Material m_neutral = null;
+    [SerializeField] private Material m_attack = null;
     [HideInInspector] public bool isInRange = false;
     private LineRenderer renderer;
+    private GameObject m_player;
+    private bool hasChangedColour = false;
     //This is for damaging the player.
     //[SerializeField] private GameObject m_player = null;
     //[SerializeField] private float damage = 1.0f;
     private void Awake()
     {
         renderer = GetComponent<LineRenderer>();
+        m_player = GameObject.FindGameObjectWithTag("Player");
     }
     // Update is called once per frame
     private void Update()
     {
-
         if (isInRange)
         {
             renderer.enabled = true;
             renderer.SetPosition(0, transform.position);
             renderer.SetPosition(1, transform.position + (transform.forward * beamLength));
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.forward, out hit, beamLength))
+            {
+                if (hit.transform.gameObject == m_player)
+                {
+                    renderer.material = m_attack;
+                    hasChangedColour = true;
+                }
+            }
+            else if (hasChangedColour)
+            {
+                renderer.material = m_neutral;
+                hasChangedColour = false;
+            }
         }
         else
             renderer.enabled = false;
