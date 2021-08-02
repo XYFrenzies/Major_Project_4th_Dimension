e48058d43e2d44e1b5946a698be4375e52981bb7
Ben <bennybill16@gmail.com>
2021-08-02 10:44:05 +1000

    Got the scanner to radiate from the outta part of the player.
     Need visuals to show that the player is radiating the scanner and that the objects are seen.

--- Major_Project_4th_Dimension/Assets/Test/Ben/Shader/ScannerEffect/DemoScannerController.cs
+++ Major_Project_4th_Dimension/Assets/Test/Ben/Shader/ScannerEffect/DemoScannerController.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class DemoScannerController : MonoBehaviour
 {
+    [SerializeField] private Transform m_scanLocation = null;
     public Material material;
     private float _scanDistance;
     public float Speed;
     bool _scanning;
-    Camera _camera;
 
     public void Scanner(InputAction.CallbackContext context) 
     {
@@ -17,12 +17,8 @@ public class DemoScannerController : MonoBehaviour
             return;
         }
         _scanning = true;
-        _scanDistance = 0;
+        _scanDistance = 3;
         material.SetFloat("_ScanDistance", _scanDistance);
-    }
-    private void Awake()
-    {
-        _camera = Camera.main;
     }
     void Update()
     {
@@ -30,6 +26,7 @@ public class DemoScannerController : MonoBehaviour
         {
             _scanDistance += Time.deltaTime * Speed;
             material.SetFloat("_ScanDistance", _scanDistance);
+            material.SetVector("_WorldSpaceScannerPos", new Vector4(m_scanLocation.position.x, m_scanLocation.position.y, m_scanLocation.position.z, 0));
         }
         //if (Input.GetMouseButtonDown(0))
         //{
