4170b4e924dd85ffff9a361f3d98a20838f2b6b0
Igor Doslov <s191719@students.aie.edu.au>
2021-08-27 10:33:02 +1000

    ID - Throw disabled for now

--- Major_Project_4th_Dimension/Assets/Scenes/TestScenes/Igor_Scenes/Test/SwitchCam.cs
+++ Major_Project_4th_Dimension/Assets/Scenes/TestScenes/Igor_Scenes/Test/SwitchCam.cs
@@ -99,8 +99,8 @@ public class SwitchCam : MonoBehaviour
             player.currentState = PlayerControllerCinemachineLook.State.HookShotThrown;
             if (!chainShoot.isObjectHeld)
                 chainShoot.currentHookShotState = ChainShootStartAgain.HookShotState.Throw;
-            else if(chainShoot.isObjectHeld)
-                chainShoot.currentHookShotState = ChainShootStartAgain.HookShotState.ThrowObject;
+            //else if(chainShoot.isObjectHeld)
+            //    chainShoot.currentHookShotState = ChainShootStartAgain.HookShotState.ThrowObject;
             else
                 chainShoot.currentHookShotState = ChainShootStartAgain.HookShotState.Place;
 
