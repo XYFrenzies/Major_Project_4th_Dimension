1825cad760d804122310f58709ad02d9988eed62
Ben <bennybill16@gmail.com>
2021-09-24 14:24:59 +1000

    BM - Drawn a fps on the top left to test out the game.

--- Major_Project_4th_Dimension/Assets/Scripts/UI/Menus/OptionsMenu.cs
+++ Major_Project_4th_Dimension/Assets/Scripts/UI/Menus/OptionsMenu.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class OptionsMenu : MonoBehaviour
 {
+    [SerializeField] private PlayerInput playerInput;
+    [SerializeField] private List<GameObject> m_menus;
+    private InputAction m_optionsMenuAction;
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_optionsMenuAction = playerInput.actions["OptionsMovement"];
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CheckInputStatus();
+    }
+
+    private void CheckInputStatus() 
+    {
+
+    }
+    public void SetMenu(GameObject menuSelected) 
+    {
+        foreach (var item in m_menus)
+        {
+            if (item == menuSelected)
+                item.SetActive(true);
+            else
+                item.SetActive(false);
+        }
     }
 }
---
+++ Major_Project_4th_Dimension/Assets/Scripts/UI/Menus/RenderingMenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderingMenu : Singleton<RenderingMenu>
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
