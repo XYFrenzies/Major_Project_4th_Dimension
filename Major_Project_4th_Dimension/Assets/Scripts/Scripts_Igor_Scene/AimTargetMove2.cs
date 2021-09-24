2989f7667d94baaeb45a0871b2e9cba138a8e99f
Igor Doslov <s191719@students.aie.edu.au>
2021-09-25 01:16:25 +1000

    ID - reducing exorcist movement WIP

---
+++ Major_Project_4th_Dimension/Assets/Scripts/Scripts_Igor_Scene/AimTargetMove2.cs
@@ -0,0 +1,69 @@
+/*
+Author:		        Igor Doslov
+Date Created:       1/4/2021
+Date Modified:      8/4/2021
+Purpose:	        Draws a line in scene view to show the ray from the player 
+                    and also makes the player aim at the target floating in front of them,
+                    which has its position lerped to make aiming movement smooth
+*/
+
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AimTargetMove2 : MonoBehaviour
+{
+    public PlayerInput playerInput;
+    private InputAction lookAction;
+    private Vector2 mouseInput;
+
+    public float weaponRange = 200.0f;
+    private Camera cam;
+    //RaycastHit hit;
+    public GameObject target;
+    public float lerpSpeed = 10.0f;
+    public LayerMask layerMask;
+    public ArmStateManager arm;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        cam = Camera.main;
+        lookAction = playerInput.actions["Look"];
+        arm = GetComponent<ArmStateManager>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        Vector3 lastPos = target.transform.position;
+
+        RaycastHit hit;
+        Vector3 aimPoint;
+
+        Ray crosshair = new Ray(cam.transform.position, cam.transform.forward);
+
+        if (Physics.Raycast(crosshair, out hit, weaponRange, ~layerMask))
+        {
+            aimPoint = hit.point;
+        }
+        else
+        {
+            aimPoint = crosshair.origin + crosshair.direction * weaponRange;
+        }
+
+        Ray ray = new Ray(arm.holdPoint.position, aimPoint - arm.holdPoint.position);
+
+        // Make the target sphere move to the point where the ray hits
+        if (Physics.Raycast(ray, out hit, weaponRange, ~layerMask))
+        {
+            target.transform.position = Vector3.Lerp(lastPos, hit.point, lerpSpeed * Time.fixedDeltaTime);
+        }
+        else // Makes the target sphere float at a point along the ray at a distance
+        {
+            target.transform.position = Vector3.Lerp(lastPos, cam.transform.forward * weaponRange, lerpSpeed * Time.fixedDeltaTime);
+        }
+
+
+    }
+}
