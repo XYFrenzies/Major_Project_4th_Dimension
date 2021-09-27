b30b6c5d8b789101819386a84b00a5d7363735cd
Ben <bennybill16@gmail.com>
2021-09-27 11:11:05 +1000

    BM - In the options menu theres a choice to draw the fps

--- Major_Project_4th_Dimension/Assets/Scripts/UI/DrawFPS.cs
+++ Major_Project_4th_Dimension/Assets/Scripts/UI/DrawFPS.cs
@@ -1,28 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class DrawFPS : MonoBehaviour
 {
-	private float deltaTime = 0.0f;
+	private float m_deltaTime = 0.0f;
+	[SerializeField] private Toggle m_check;
 	private void Update()
 	{
-		deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
+		if (CheckValue())
+			m_deltaTime += (Time.unscaledDeltaTime - m_deltaTime) * 0.1f;
 	}
 
 	private void OnGUI()
 	{
-		int w = Screen.width, h = Screen.height;
+		if (CheckValue())
+		{
+			int w = Screen.width, h = Screen.height;
 
-		GUIStyle style = new GUIStyle();
+			GUIStyle style = new GUIStyle();
 
-		Rect rect = new Rect(0, 0, w, h * 2 / 100);
-		style.alignment = TextAnchor.UpperLeft;
-		style.fontSize = h * 5 / 100;
-		style.normal.textColor = new Color(1.0f, 1.0f, 0.5f, 1.0f);
-		float msec = deltaTime * 1000.0f;
-		float fps = 1.0f / deltaTime;
-		string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
-		GUI.Label(rect, text, style);
+			Rect rect = new Rect(0, 0, w, h * 2 / 100);
+			style.alignment = TextAnchor.UpperLeft;
+			style.fontSize = h * 5 / 100;
+			style.normal.textColor = new Color(1.0f, 1.0f, 0.5f, 1.0f);
+			float msec = m_deltaTime * 1000.0f;
+			float fps = 1.0f / m_deltaTime;
+			string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
+			GUI.Label(rect, text, style);
+		}
+	}
+
+	private bool CheckValue()
+	{
+		if (m_check.isOn)
+			return true;
+		else
+			return false;
 	}
 }
