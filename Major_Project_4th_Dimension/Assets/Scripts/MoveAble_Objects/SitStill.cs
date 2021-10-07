f5c99faf4e01774719a07a680ac8c17f19ed1aa0
Ben <bennybill16@gmail.com>
2021-10-07 12:40:03 +1100

    BM - I have got a half fix for the container falling on the ground.

---
+++ Major_Project_4th_Dimension/Assets/Scripts/MoveAble_Objects/SitStill.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SitStill : MonoBehaviour
+{
+    [SerializeField] private float m_timeBeforeFreeze = 2.0f;
+    private void OnTriggerEnter(Collider other)
+    {
+        if((other.CompareTag("BigPullObject") || other.CompareTag("MoveableToMe")) && other.GetComponent<Rigidbody>().constraints != RigidbodyConstraints.FreezeRotation)
+            StartCoroutine(Timer(other));
+    }
+    private IEnumerator Timer(Collider col) 
+    {
+        yield return new WaitForSeconds(m_timeBeforeFreeze);
+        col.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+    }
+}
