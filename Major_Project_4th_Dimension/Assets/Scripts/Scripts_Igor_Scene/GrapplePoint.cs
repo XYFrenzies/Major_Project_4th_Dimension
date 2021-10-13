38e838fe5da5fb17e180bf31394a16906066a3be
Ben <bennybill16@gmail.com>
2021-10-14 10:11:00 +1100

    BM - Working on the UI of the game

--- Major_Project_4th_Dimension/Assets/Scripts/Scripts_Igor_Scene/GrapplePoint.cs
+++ Major_Project_4th_Dimension/Assets/Scripts/Scripts_Igor_Scene/GrapplePoint.cs
@@ -5,18 +5,15 @@ using UnityEngine.Events;
 
 public class GrapplePoint : MonoBehaviour
 {
-    Transform cam;
     Material mat;
-    PlayerController playerCont;
     public int id;
     public Material materialOn;
     public Material materialOff;
-
+    [SerializeField] private GameObject m_interfaceHookPC;
+    [SerializeField] private GameObject m_interfaceHookXbox;
     private void Awake()
     {
         mat = GetComponent<Renderer>().material;
-        cam = Camera.main.transform;
-
     }
 
     private void Start()
@@ -24,25 +21,20 @@ public class GrapplePoint : MonoBehaviour
         GameEvents.current.onGrapplePointVisible += TurnPointOn;
         GameEvents.current.onGrapplePointNotVisible += TurnPointOff;
     }
-
-    private void Update()
-    {
-
-    }
-
     public void TurnPointOn(int id)
     {
         if (id == this.id)
-        {
             mat.color = materialOn.color;
-        }
+        m_interfaceHookPC.SetActive(true);
     }
-
+    private void Update()
+    {
+        m_interfaceHookPC.transform.LookAt(GameObject.FindGameObjectWithTag("Player").transform);
+        m_interfaceHookXbox.transform.LookAt(GameObject.FindGameObjectWithTag("Player").transform);
+    }
     public void TurnPointOff()
     {
-        
             mat.color = materialOff.color;
-     
+        m_interfaceHookPC.SetActive(false);
     }
-
 }
