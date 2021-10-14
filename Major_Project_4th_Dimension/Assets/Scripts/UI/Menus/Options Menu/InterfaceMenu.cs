83c3da5696fadfc0b78486c02cc7c3c7cc149ee4
Ben <bennybill16@gmail.com>
2021-10-15 09:43:21 +1100

    BM - Updating scrips for alpha submission

--- Major_Project_4th_Dimension/Assets/Scripts/UI/Menus/Options Menu/InterfaceMenu.cs
+++ Major_Project_4th_Dimension/Assets/Scripts/UI/Menus/Options Menu/InterfaceMenu.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+/// <summary>
+/// An interface class to set variables between scenes and startups.
+/// </summary>
 public class InterfaceMenu : MonoBehaviour
 {
     [SerializeField] private Toggle fpsCounter;
     private bool valueFPSCounter = false;
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         valueFPSCounter = GlobalVariables.Instance.GetFPSIsOn();
         fpsCounter.isOn = valueFPSCounter;
