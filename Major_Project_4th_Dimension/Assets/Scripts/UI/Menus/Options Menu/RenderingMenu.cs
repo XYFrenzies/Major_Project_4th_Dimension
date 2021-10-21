d4e57022c9816fd0ff17f4794915648b96f7d623
Ben <bennybill16@gmail.com>
2021-10-21 17:33:49 +1100

    BM - I have fixed the rendering and resolution issues.

--- Major_Project_4th_Dimension/Assets/Scripts/UI/Menus/Options Menu/RenderingMenu.cs
+++ Major_Project_4th_Dimension/Assets/Scripts/UI/Menus/Options Menu/RenderingMenu.cs
@@ -20,15 +20,24 @@ public class RenderingMenu : Singleton<RenderingMenu>
         List<string> options = new List<string>();
         for (int i = 0; i < m_resolutionsMultiple.Length; i++)
         {
+            int value = i - 1;
             string option = m_resolutionsMultiple[i].width + " x " + m_resolutionsMultiple[i].height;
-            options.Add(option);
+            if (value != -1)
+            {
+                if (((m_resolutionsMultiple[i].width != m_resolutionsMultiple[i - 1].width &&
+                    m_resolutionsMultiple[i].height != m_resolutionsMultiple[i - 1].height) || m_resolutionsMultiple[i].width == m_resolutionsMultiple[i - 1].width &&
+                    m_resolutionsMultiple[i].height != m_resolutionsMultiple[i - 1].height) || m_resolutionsMultiple[i].width != m_resolutionsMultiple[i - 1].width &&
+                    m_resolutionsMultiple[i].height == m_resolutionsMultiple[i - 1].height)
+                    options.Add(option);
+            }
+            if (i == 0)
+                options.Add(option);
             if (m_resolutionsMultiple[i].width == Screen.currentResolution.width &&
                 m_resolutionsMultiple[i].height == Screen.currentResolution.height)
             {
                 indexOfResolution = i;
             }
         }
-        
         m_resolutionDropDown.AddOptions(options);
         m_quality.ClearOptions();
         List<string> qualityOptions = new List<string>();
@@ -38,17 +47,16 @@ public class RenderingMenu : Singleton<RenderingMenu>
             string option = m_qualityChanger[i].name;
             qualityOptions.Add(option);
         }
-        indexOfResolution = GlobalVariables.Instance.m_resolutionInt;
+        m_resolutionLevel = indexOfResolution;
         m_qualityLevel = GlobalVariables.Instance.m_qualityDisplayInt;
         m_quality.AddOptions(qualityOptions);
         m_resolutionDropDown.value = indexOfResolution;
         m_resolutionDropDown.RefreshShownValue();
-        m_quality.value = 2;
+        m_quality.value = m_qualityLevel;
         m_quality.RefreshShownValue();
     }
     public void SaveValues()
     {
-        GlobalVariables.Instance.m_qualityDisplayInt = m_qualityLevel;
         Resolution resolution = m_resolutionsMultiple[m_resolutionLevel];
         Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
         QualitySettings.SetQualityLevel(m_qualityLevel);
