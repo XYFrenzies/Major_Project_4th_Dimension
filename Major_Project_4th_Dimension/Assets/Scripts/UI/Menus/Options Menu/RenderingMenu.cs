57a24e7030963cbd2628b67d7c2f621e14609d17
Ben <bennybill16@gmail.com>
2021-10-21 15:21:19 +1100

    BM - Rendering in options menu works

--- Major_Project_4th_Dimension/Assets/Scripts/UI/Menus/Options Menu/RenderingMenu.cs
+++ Major_Project_4th_Dimension/Assets/Scripts/UI/Menus/Options Menu/RenderingMenu.cs
@@ -7,11 +7,13 @@ public class RenderingMenu : Singleton<RenderingMenu>
     public TMP_Dropdown m_resolutionDropDown;
     public TMP_Dropdown m_quality;
     private Resolution[] m_resolutionsMultiple;
-    [SerializeField]private HDRenderPipelineAsset[] m_qualityChanger;
+    [SerializeField] private HDRenderPipelineAsset[] m_qualityChanger;
     private int m_qualityLevel;
+    private int m_resolutionLevel;
     // Start is called before the first frame update
     private void Start()
     {
+        int indexOfResolution = 0;
         int amountOFQuality = m_qualityChanger.Length;
         m_resolutionsMultiple = Screen.resolutions;
         m_resolutionDropDown.ClearOptions();
@@ -20,21 +22,20 @@ public class RenderingMenu : Singleton<RenderingMenu>
         {
             string option = m_resolutionsMultiple[i].width + " x " + m_resolutionsMultiple[i].height;
             options.Add(option);
-        }
-        m_resolutionDropDown.AddOptions(options);
-        m_quality.ClearOptions();
-        List<string> qualityOptions = new List<string>();
-        int indexOfResolution = 0;
-        for (int i = 0; i < amountOFQuality; i++)
-        {
-            string option = m_qualityChanger[i].name;
-            qualityOptions.Add(option);
-
             if (m_resolutionsMultiple[i].width == Screen.currentResolution.width &&
                 m_resolutionsMultiple[i].height == Screen.currentResolution.height)
             {
                 indexOfResolution = i;
             }
+        }
+        m_resolutionDropDown.AddOptions(options);
+        m_quality.ClearOptions();
+        List<string> qualityOptions = new List<string>();
+
+        for (int i = 0; i < amountOFQuality; i++)
+        {
+            string option = m_qualityChanger[i].name;
+            qualityOptions.Add(option);
         }
         m_quality.AddOptions(qualityOptions);
         m_resolutionDropDown.value = indexOfResolution;
@@ -43,18 +44,19 @@ public class RenderingMenu : Singleton<RenderingMenu>
     public void SaveValues()
     {
         GlobalVariables.Instance.m_qualityDisplayInt = m_qualityLevel;
-    }
-    public void SetResolution(int resolutionIndex) 
-    {
-        Resolution resolution = m_resolutionsMultiple[resolutionIndex];
+        Resolution resolution = m_resolutionsMultiple[m_resolutionLevel];
         Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+        QualitySettings.SetQualityLevel(m_qualityLevel);
     }
-    public void SetQuality(int qualityIndex) 
+    public void SetResolution(int resolutionIndex)
+    {
+        m_resolutionLevel = resolutionIndex;
+    }
+    public void SetQuality(int qualityIndex)
     {
         m_qualityLevel = qualityIndex;
-        QualitySettings.SetQualityLevel(qualityIndex);
     }
-    public void SetFullScreen(bool isFullScreen) 
+    public void SetFullScreen(bool isFullScreen)
     {
         Screen.fullScreen = isFullScreen;
     }
