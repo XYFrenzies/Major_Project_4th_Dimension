460caf448123bc6548fac10904f17bd91f814939
Ben <bennybill16@gmail.com>
2021-10-27 16:50:49 +1100

    BM - I have been working on the UI manager WIP

--- Major_Project_4th_Dimension/Assets/Scripts/UI/Tutorial/UI_Tutorial/FourthRoomUIPullObjects.cs
+++ Major_Project_4th_Dimension/Assets/Scripts/UI/Tutorial/UI_Tutorial/FourthRoomUIPullObjects.cs
@@ -4,25 +4,13 @@ using UnityEngine;
 
 public class FourthRoomUIPullObjects : MonoBehaviour
 {
-    private GameObject m_keyWASDImage;
-    private GameObject m_joySticks;
-    private GameObject m_lookAroundText;
-    private GameObject m_pressToShoot;
-    private GameObject m_zoomToLook;
+    private GameObject m_pullObjects;
     private List<GameObject> allObjectsInUI;
     // Start is called before the first frame update
     void Start()
     {
-        m_keyWASDImage = gameObject.transform.Find("Canvas").Find("WASD Image").gameObject;
-        m_joySticks = gameObject.transform.Find("Canvas").Find("JoyStick").gameObject;
-        m_lookAroundText = gameObject.transform.Find("Canvas").Find("Look Around").gameObject;
-        m_pressToShoot = gameObject.transform.Find("Canvas").Find("Left_Click").gameObject;
-        m_zoomToLook = gameObject.transform.Find("Canvas").Find("Hold_Right_Click").gameObject;
-        allObjectsInUI.Add(m_keyWASDImage);
-        allObjectsInUI.Add(m_joySticks);
-        allObjectsInUI.Add(m_lookAroundText);
-        allObjectsInUI.Add(m_pressToShoot);
-        allObjectsInUI.Add(m_zoomToLook);
+        m_pullObjects = gameObject.transform.Find("Canvas").Find("Pull Objects").gameObject;
+        allObjectsInUI.Add(m_pullObjects);
     }
 
     private void OnTriggerEnter(Collider other)
