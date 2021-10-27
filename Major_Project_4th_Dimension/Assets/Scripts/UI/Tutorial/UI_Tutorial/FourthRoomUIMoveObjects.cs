f32cb7283da882e01b10082eb5a30daa97c43205
Ben <bennybill16@gmail.com>
2021-10-27 16:46:59 +1100

    BM - I am making a manager for the UI for tutorial

---
+++ Major_Project_4th_Dimension/Assets/Scripts/UI/Tutorial/UI_Tutorial/FourthRoomUIMoveObjects.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FourthRoomUIMoveObjects : MonoBehaviour
+{
+    private GameObject m_keyWASDImage;
+    private GameObject m_joySticks;
+    private GameObject m_lookAroundText;
+    private GameObject m_pressToShoot;
+    private GameObject m_zoomToLook;
+    private List<GameObject> allObjectsInUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_keyWASDImage = gameObject.transform.Find("Canvas").Find("WASD Image").gameObject;
+        m_joySticks = gameObject.transform.Find("Canvas").Find("JoyStick").gameObject;
+        m_lookAroundText = gameObject.transform.Find("Canvas").Find("Look Around").gameObject;
+        m_pressToShoot = gameObject.transform.Find("Canvas").Find("Left_Click").gameObject;
+        m_zoomToLook = gameObject.transform.Find("Canvas").Find("Hold_Right_Click").gameObject;
+        allObjectsInUI.Add(m_keyWASDImage);
+        allObjectsInUI.Add(m_joySticks);
+        allObjectsInUI.Add(m_lookAroundText);
+        allObjectsInUI.Add(m_pressToShoot);
+        allObjectsInUI.Add(m_zoomToLook);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) 
+        {
+            UIManager.Instance.EnableObjects(true, allObjectsInUI);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            UIManager.Instance.EnableObjects(false, allObjectsInUI);
+        }
+    }
+}
--- Major_Project_4th_Dimension/Assets/Scripts/UI/Tutorial/UI_Tutorial/ThirdRoomUIScanner.cs
+++ Major_Project_4th_Dimension/Assets/Scripts/UI/Tutorial/UI_Tutorial/ThirdRoomUIScanner.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UIHelper : MonoBehaviour
+public class ThirdRoomUIScanner : MonoBehaviour
 {
-    [SerializeField] private GameObject m_startingArea;
     private GameObject m_keyWASDImage;
     private GameObject m_joySticks;
     private GameObject m_lookAroundText;
     private GameObject m_pressToShoot;
     private GameObject m_zoomToLook;
+    private List<GameObject> allObjectsInUI;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,11 +18,25 @@ public class UIHelper : MonoBehaviour
         m_lookAroundText = gameObject.transform.Find("Canvas").Find("Look Around").gameObject;
         m_pressToShoot = gameObject.transform.Find("Canvas").Find("Left_Click").gameObject;
         m_zoomToLook = gameObject.transform.Find("Canvas").Find("Hold_Right_Click").gameObject;
+        allObjectsInUI.Add(m_keyWASDImage);
+        allObjectsInUI.Add(m_joySticks);
+        allObjectsInUI.Add(m_lookAroundText);
+        allObjectsInUI.Add(m_pressToShoot);
+        allObjectsInUI.Add(m_zoomToLook);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player")) 
+        {
+            UIManager.Instance.EnableObjects(true, allObjectsInUI);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            UIManager.Instance.EnableObjects(false, allObjectsInUI);
+        }
     }
 }
