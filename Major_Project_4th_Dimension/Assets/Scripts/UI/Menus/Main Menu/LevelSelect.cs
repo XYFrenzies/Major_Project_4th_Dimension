36625188b4bc0b7e18e3fbce7550793a7b1ebfeb
Ben <bennybill16@gmail.com>
2021-10-28 10:51:28 +1100

    BM - Made a change in main menu, got the animation started.

--- Major_Project_4th_Dimension/Assets/Scripts/UI/Menus/Main Menu/LevelSelect.cs
+++ Major_Project_4th_Dimension/Assets/Scripts/UI/Menus/Main Menu/LevelSelect.cs
@@ -5,10 +5,21 @@ using UnityEngine.SceneManagement;
 /// <summary>
 /// Any designer can use this to select the name of the level to proceed to next.
 /// </summary>
+/// //playGame
 public class LevelSelect : MonoBehaviour
 {
+    [SerializeField] private Animator m_playerAnimtion;
+    [SerializeField] private float m_timeBeforeStartScene;
     public void LevelToSelect(string name) 
     {
+        if(m_playerAnimtion != null)
+            m_playerAnimtion.SetBool("playGame", true);
+        gameObject.SetActive(false);
+        StartCoroutine(LevelBeforeStart(name));
+    }
+    private IEnumerator LevelBeforeStart(string name) 
+    {
+        yield return new WaitForSeconds(m_timeBeforeStartScene);
         SceneManager.LoadScene(name);
     }
 }
