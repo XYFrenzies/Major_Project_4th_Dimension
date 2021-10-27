e7021c5a284c6255ca45edf6289a58c2dde206c6
Ben <bennybill16@gmail.com>
2021-10-27 12:07:03 +1100

    BM - I have started doing the lights in tutorial level

--- Major_Project_4th_Dimension/Assets/Scripts/LightController/LightController.cs
+++ Major_Project_4th_Dimension/Assets/Scripts/LightController/LightController.cs
@@ -1,13 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class LightToDoor
-{
-    public string spotLights { get; set; }
-    public Light spotLightInScene;
-    public Light pointLightInScene;
-    public int eventID;
-}
+
 public class LightController : Singleton<LightController>
 {
     [SerializeField] private List<LightToDoor> lightLogic;
@@ -20,22 +14,5 @@ public class LightController : Singleton<LightController>
             lightLogic[i].spotLightInScene.color = m_defaultColour;
             lightLogic[i].pointLightInScene.color = m_defaultColour;
         }
-    }
-    public void SetIDToColour(int id, Color colour)
-    {
-        if (FindID(id))
-        {
-            lightLogic[id].pointLightInScene.color = colour;
-            lightLogic[id].spotLightInScene.color = colour;
-        }
-    }
-    private bool FindID(int id)
-    {
-        for (int i = 0; i < lightLogic.Count; i++)
-        {
-            if (lightLogic[i].eventID == id)
-                return true;
-        }
-        return false;
     }
 }
