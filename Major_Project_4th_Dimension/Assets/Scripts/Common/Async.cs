710207361334e90bcbe91a6ef1ec5d3eaa2798be
Ben <bennybill16@gmail.com>
2021-10-28 16:59:28 +1100

    BM - Last minute changes to the scripts before tomorrow

--- Major_Project_4th_Dimension/Assets/Scripts/Common/Async.cs
+++ Major_Project_4th_Dimension/Assets/Scripts/Common/Async.cs
@@ -4,19 +4,25 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class Async : MonoBehaviour
 {
+    private List<AsyncOperation> asyncList;
     // Start is called before the first frame update
     private void Start()
     {
+
         for (int i = 0; i < SceneManager.sceneCountInBuildSettings; i++)
         {
-                SceneManager.LoadSceneAsync(i);
-                SceneManager.GetSceneByBuildIndex(i).GetRootGameObjects()[0].SetActive(false);
+            AsyncOperation newAsync = SceneManager.LoadSceneAsync(i);
+            asyncList.Add(newAsync);
+            newAsync.allowSceneActivation = false;
+            SceneManager.GetSceneByBuildIndex(i).GetRootGameObjects()[0].SetActive(false);
         }
         SceneManager.GetSceneByName("MainMenu").GetRootGameObjects()[0].SetActive(true);
     }
-    public void OpenNewScene(int i) 
+    public void OpenNewScene(int i)
     {
-        SceneManager.GetActiveScene().GetRootGameObjects()[0].SetActive(false);
-        SceneManager.GetSceneByBuildIndex(i).GetRootGameObjects()[0].SetActive(true);
+        for (int index = 0; index < asyncList.Count; index++)
+        {
+            asyncList[index].allowSceneActivation = true;
+        }
     }
 }
