c2d7435819d3630edc6664b35cdcd1d94c1044bc
Ben <bennybill16@gmail.com>
2021-11-03 19:20:26 +1100

    BM - Start of defining the axis.

--- Major_Project_4th_Dimension/Assets/Scripts/UI/Menus/Options Menu/MenuInput.cs
+++ Major_Project_4th_Dimension/Assets/Scripts/UI/Menus/Options Menu/MenuInput.cs
@@ -1,11 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Cinemachine;
 public class MenuInput : MonoBehaviour
 {
-     //Sensitivety X
-     //Sensitivety Y
+    private CinemachineVirtualCamera player;
+    private float xAxis;
+    private float yAxis;
+    //Sensitivety X
+    //Sensitivety Y
+    private void Awake()
+    {
+        if(GameObject.FindGameObjectWithTag("PlayerFlag").transform.Find("3rdPersonCinemachine").GetComponent<CinemachineVirtualCamera>() != null)
+            player = GameObject.FindGameObjectWithTag("PlayerFlag").transform.Find("3rdPersonCinemachine").GetComponent<CinemachineVirtualCamera>();
+        xAxis = GlobalVariables.Instance.verticalSensitivity;
+        yAxis = GlobalVariables.Instance.horizontalSensitivity;
+        //player. ;
+    }
     public void ControllerInput() 
     {
         CheckInput.Instance.SetController();
