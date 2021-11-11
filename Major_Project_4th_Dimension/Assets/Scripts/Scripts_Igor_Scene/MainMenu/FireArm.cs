f1ae420d8a7ec579d1eda75c218bd8803c23e6f9
Igor Doslov <igordoslov@protonmail.com>
2021-11-12 09:41:33 +1100

    ID - main menu animations working again

--- Major_Project_4th_Dimension/Assets/Scripts/Scripts_Igor_Scene/MainMenu/FireArm.cs
+++ Major_Project_4th_Dimension/Assets/Scripts/Scripts_Igor_Scene/MainMenu/FireArm.cs
@@ -4,12 +4,10 @@ using UnityEngine;
 
 public class FireArm : MonoBehaviour
 {
-    //public bool isArmOn = false;
     public LineRenderer lineRenderer;
 
     public void FireTheArm()
     {
-        //isArmOn = true;
         lineRenderer.enabled = true;
     }
 }
