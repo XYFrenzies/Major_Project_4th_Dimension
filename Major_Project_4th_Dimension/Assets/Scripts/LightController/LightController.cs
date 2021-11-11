5b7350032af522808111ed4e39957ff178af87dc
Ben <bennybill16@gmail.com>
2021-11-11 16:28:42 +1100

    BM - Deleting variables in script

--- Major_Project_4th_Dimension/Assets/Scripts/LightController/LightController.cs
+++ Major_Project_4th_Dimension/Assets/Scripts/LightController/LightController.cs
@@ -4,37 +4,4 @@ using UnityEngine;
 
 public class LightController : Singleton<LightController>
 {
-    [SerializeField] private List<LightToDoor> lightLogic;
-
-    /// <summary>
-    /// colo
-    /// </summary>
-    /// <param name="id"></param>
-    /// <param name="colour"> is the number for which corresponds to the colour value
-    ///  green = 0
-    ///  yellow = 1
-    ///  red = 2</param>
-    public void SetColourID(int id, int colour)
-    {
-        for (int i = 0; i < lightLogic.Count; i++)
-        {
-            if (lightLogic[i].eventID == id)
-            {
-                if (colour > 2 || colour < 0)
-                    return;
-                switch (colour)
-                {
-                    case 0:
-                        lightLogic[i].SetIDToGreen();
-                        break;
-                    case 1:
-                        lightLogic[i].SetIDToYellow();
-                        break;
-                    case 2:
-                        lightLogic[i].SetIDToRed();
-                        break;
-                }
-            }
-        }
-    }
 }
