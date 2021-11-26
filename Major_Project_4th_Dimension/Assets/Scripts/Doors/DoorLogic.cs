b8a75420db0092830e5c53084ea1f0b7753d11b7
Ben <bennybill16@gmail.com>
2021-11-26 11:35:45 +1100

    BM - Forgot to move the player back to the 1st level again.

--- Major_Project_4th_Dimension/Assets/Scripts/Doors/DoorLogic.cs
+++ Major_Project_4th_Dimension/Assets/Scripts/Doors/DoorLogic.cs
@@ -6,15 +6,16 @@ public class DoorLogic : MonoBehaviour
 {
     [SerializeField] private float openSpeed = 5f;
     [SerializeField] private float closeSpeed = 5f;
-    private Vector3 target;
     [Space]
     [SerializeField] private float xAxisAmount = 0f;
     [SerializeField] private float yAxisAmount = 0f;
     [SerializeField] private float zAxisAmount = 0f;
+    [SerializeField] private DoorData doorData;
+    private Vector3 target;
     private Vector3 startPos;
     private bool open = false;
     private bool close = false;
-    [SerializeField] private DoorData doorData;
+
     private void Awake()
     {
         target = new Vector3(transform.position.x + xAxisAmount, transform.position.y + yAxisAmount, transform.position.z + zAxisAmount);
@@ -44,6 +45,5 @@ public class DoorLogic : MonoBehaviour
         close = true;
         open = false;
         transform.position = Vector3.MoveTowards(transform.position, startPos, closeSpeed * Time.deltaTime);
-
     }
 }
